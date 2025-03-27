e8f450a689249181f9f778f6364282b1d0352423
Abidha shree R <128830560+Abidhashree03@users.noreply.github.com>
2025-03-27 10:01:13 +0530

    Create App.jsx

---
+++ level-2/task-9/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DynamicText from "./UserMessage";
+
+const App = () => {
+  return (
+    <div>
+      <DynamicText />
+    </div>
+  );
+};
+
+export default App;
