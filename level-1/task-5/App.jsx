04702760748e49eec4edafadfc9352b4aaf8cc00
Abidha shree R <128830560+Abidhashree03@users.noreply.github.com>
2025-03-27 09:22:25 +0530

    Create App.jsx

---
+++ level-1/task-5/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+const Hello = () => {
+  const num1 = 10;
+  const num2 = 20;
+
+  return (
+    <div>
+<h1>THE SUM OF TWO NUMBERS</h1>            
+<p>The sum of {num1} and {num2} is: {num1 + num2}</p>
+    </div>
+  );
+};
+
+export default Hello;
