648b274d601789c2fbad39596610c94ed8073938
Abidha shree R <128830560+Abidhashree03@users.noreply.github.com>
2025-03-27 10:15:42 +0530

    Create App.jsx

---
+++ level-3/task-2/App.jsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+import MultiFieldForm from "./UserMessage";
+
+const App = () => {
+  return (
+    <div>
+      <MultiFieldForm />
+    </div>
+  );
+};
+
+export default App;
