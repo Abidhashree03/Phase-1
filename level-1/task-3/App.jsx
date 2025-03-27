c978109451ca5514e0fb4209f2884bcf895bec6c
Abidha shree R <128830560+Abidhashree03@users.noreply.github.com>
2025-03-27 09:07:00 +0530

    Create App.jsx

---
+++ level-1/task-3/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Hello = () => {
+return (
+    <div>
+      <h1>HELLO ABIDHA SHREE!</h1>
+      <p>I AM A "B.TECH INFORMATION TECHNOLOGY" STUDENT</p>
+    </div>
+);
+};
+
+export default Hello;
