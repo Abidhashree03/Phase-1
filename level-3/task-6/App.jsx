a68e72c5c1c5aacf9116f589358dca3ba32fa231
Abidha shree R <128830560+Abidhashree03@users.noreply.github.com>
2025-03-27 10:27:25 +0530

    Create App.jsx

---
+++ level-3/task-6/App.jsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+import SearchFilterList from "./UserMessage";
+
+const App = () => {
+  return (
+    <div>
+      <SearchFilterList />
+    </div>
+  );
+};
+
+export default App;
