84498490975ae8176efb169334e395aab7c24d7f
Abidha shree R <128830560+Abidhashree03@users.noreply.github.com>
2025-03-27 10:06:41 +0530

    Create App.jsx

---
+++ level-2/task-11/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FormComponent from "./UserMessage";
+
+const App = () => {
+  return (
+    <div>
+      <FormComponent />
+    </div>
+  );
+};
+
+export default App;
