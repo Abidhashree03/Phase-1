0763ff02b4deec93d55a18bb6f1f9d26bf12f9ae
Abidha shree R <128830560+Abidhashree03@users.noreply.github.com>
2025-03-27 09:18:50 +0530

    Create App.jsx

---
+++ level-1/task-4/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+const Hello = () => {
+ 
+  return (
+    <div>
+      <h1>HELLO ABIDHA SHREE!</h1>
+      <p>I AM A "B.TECH INFORMATION TECHNOLOGY" STUDENT</p>
+      <img 
+        src="abi.jpg" 
+        alt="Beauty Queen" 
+        style={{ width: "300px", height: "auto", borderRadius: "10px" }} 
+      />
+         </div>
+  );
+};
+
+export default Hello;
