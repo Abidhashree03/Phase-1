b791dc11f26775400a609ef2538f586a2a03771b
Abidha shree R <128830560+Abidhashree03@users.noreply.github.com>
2025-03-27 09:41:11 +0530

    Create App.jsx

---
+++ level-2/task-3/App.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+import Greeting from "./UserMessage";
+
+function App() {
+  return (
+    <div>
+      <Greeting name="Abidha Shree" />
+    </div>
+  );
+}
+
+export default App;
