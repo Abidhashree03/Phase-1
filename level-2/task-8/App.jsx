237fa229fc6436ef8233eb1f0998c4cc6707878a
Abidha shree R <128830560+Abidhashree03@users.noreply.github.com>
2025-03-27 09:57:54 +0530

    Create App.jsx

---
+++ level-2/task-8/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ToggleVisiblity from "./UserMessage";
+const App = () => {
+  return (
+    <div>
+      <ToggleVisiblity />
+    </div>
+  );
+};
+
+export default App;
