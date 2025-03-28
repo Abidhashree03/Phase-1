1ac626dd8ae2ddfaa71b8f1a6392073dae110e9e
Abidha shree R <128830560+Abidhashree03@users.noreply.github.com>
2025-03-29 00:07:31 +0530

    Create App.jsx

---
+++ level-9/task-1/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import "./App.css"; // Import the CSS file
+
+const App = () => {
+  return (
+    <Router>
+      <div className="container">
+        {/* Navigation Bar */}
+        <nav className="navbar">
+          <div className="nav-links">
+            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/about" className="nav-link">About</Link>
+            <Link to="/contact" className="nav-link">Contact</Link>
+          </div>
+        </nav>
+
+        {/* Page Content */}
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
