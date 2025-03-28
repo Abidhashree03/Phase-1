5e8b3fa2917048c7201a03de311e4751cbc5fb5b
Abidha shree R <128830560+Abidhashree03@users.noreply.github.com>
2025-03-28 10:46:34 +0530

    Create recipeData.jsx

---
+++ level-5/task-4/recipeData.jsx
@@ -0,0 +1,25 @@
+const recipes = [
+  {
+    id: "1",
+    title: "chetinadu chicken",
+    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj8wSXqniaJSPZsAoIATOurkK2DnVnY8-iyg&s",
+    ingredients: ["Spaghetti", "Eggs", "Bacon", "Parmesan", "Black Pepper"],
+    instructions: "Boil pasta. Cook bacon. Mix eggs and Parmesan. Combine everything."
+  },
+  {
+    id: "2",
+    title: "mutton Curry",
+    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcrklJPYnrJ6iZ8BFyc2izviqStcS1qGMwPA&s",
+    ingredients: ["Chicken", "Coconut Milk", "Curry Powder", "Garlic", "Onion"],
+    instructions: "Sauté onions. Add chicken and spices. Pour coconut milk. Simmer."
+  },
+  {
+    id: "3",
+    title: "chicken Dum Biryani",
+    imageUrl: "https://vismaifood.com/storage/app/uploads/public/e12/7b7/127/thumb__1200_0_0_0_auto.jpg",
+    ingredients: ["chicken", "rice", "Lemon", "Salt", "Green chilly"],
+    instructions: "First Put some oil and add onion,tomato and spices after few minutes add chicken and rice ,take a measuring cup add water.Wait few minutes biryani is ready"
+  }
+];
+
+export default recipes;
